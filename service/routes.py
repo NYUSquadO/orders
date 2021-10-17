8bb18cf028f0a380a57e37fce5d121b1bb7f305e
Sayali Shukla <sayalishukla100@gmail.com>
2021-10-17 19:33:35 -0400

    adding root url info

--- service/routes.py
+++ service/routes.py
@@ -9,6 +9,9 @@ import os
 import sys
 import logging
 from flask import Flask, jsonify, request, url_for, make_response, abort
+import werkzeug
+from werkzeug.exceptions import *
+
 from . import status  # HTTP Status Codes
 
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
@@ -26,7 +29,11 @@ from . import app
 def index():
     """ Root URL response """
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="Orders REST API Service",
+            version="1.0",
+            paths=url_for("list_orders", _external=True),
+        ),
         status.HTTP_200_OK,
     )
 
